da42412393578bbd67d93d8fb22d283adf1fe60d
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-05-13 12:40:38 +0200

    Added som minor stuff and implemented the use of the Services component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <template>
   <div id="app">
     <section id="mainsection">
-      <span>
+      <div id="nav">
         <h1 id="jakobyte">JAKOBYTE</h1>
-        <div id="nav">
-          <router-link to="/">Home</router-link>|
-          <router-link to="/about">About</router-link>
-        </div>
-      </span>
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>
+      </div>
 
       <router-view />
     </section>
 
-    <section>
-      <h2>hej</h2>
+    <section id="servicesection">
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita saepe veritatis
+        sunt repellat voluptates, tenetur consequatur perspiciatis doloribus
+        possimus omnis magnam facere in ipsa fugiat nulla deleniti ex
+        magni quis!
+      </h1>
+      <div style="display:flex; flex-direction: row;">
+        <Services msg="Hej" description="Här är lite information" />
+        <Services msg="Hej" description="Här är lite information" />
+        <i class="fab fa-android"></i>
+      </div>
     </section>
   </div>
 </template>
+
+<script>
+import Services from "./components/Services.vue";
+
+export default {
+  name: "app",
+  components: {
+    Services
+  }
+};
+</script>
 
 <style>
 @import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css";
@@ -33,10 +52,13 @@
 }
 #mainsection {
   background-color: #42b983;
-  position: fixed;
-  width: 100%;
-  height: 70%;
+  margin-bottom: 4em;
+  padding-bottom: 3em;
 }
+#servicesection.div {
+  margin: 4em;
+}
+
 .page.h1 {
   position: fixed;
   width: inherit;
